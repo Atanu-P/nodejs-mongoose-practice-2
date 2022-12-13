63dfa7a4605bdbb173df6cd7e9a914c0d206907d
Atanu Pal <atanupal@live.com>
2022-12-13 23:48:17 +0530

    read file created

---
+++ read.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const User = require("./model/User");
+
+console.log(User);
+
+async function main() {
+  const db_url = "mongodb://localhost:27017/mongoose_test";
+
+  try {
+    // Connect to the MongoDB cluster
+    await mongoose.connect(db_url);
+
+    // Find the listing by name
+    await findOneListingByName("Jen");
+
+    // Find listings by city name
+    await findListingByCity("California");
+
+    // Find all the listings in between range of age
+    await findListingByAge(10, 20);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+
+async function findOneListingByName(nameOfListing) {
+  const result = await User.findOne({ name: nameOfListing });
+  if (result) {
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+async function findListingByCity(nameOfListing) {
+  const result = await User.find({ city: nameOfListing });
+  if (result) {
+    console.log(result);
+  } else {
+    console.log(`No listings found with the city '${nameOfListing}'`);
+  }
+}
+
+async function findListingByAge(lowerlimit, upperLimit) {
+  const result = await User.find({
+    age: { $gte: lowerlimit, $lte: upperLimit },
+  });
+  if (result.length > 0) {
+    console.log(result);
+  } else {
+    console.log(
+      `No listings found within age ${lowerlimit} and age ${upperLimit}.`
+    );
+  }
+}
