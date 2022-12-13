76e950fb29934ea34c6108b493f589993d785949
Atanu Pal <atanupal@live.com>
2022-12-13 19:20:33 +0530

    find data from mongodb

--- app.js
+++ app.js
@@ -11,9 +11,44 @@ async function main() {
   try {
     // Connect to the MongoDB cluster
     await mongoose.connect(db_url);
+
+    // await findOneListingByName("Jen");
+    // await findListingByCity("California");
+    await findListingByAge(10, 20);
   } catch (error) {
     console.log(error);
   }
 }
 
 main();
+
+async function findOneListingByName(nameOfListing) {
+  const result = await User.findOne({ name: nameOfListing });
+  if (result) {
+    console.log(result);
+  } else {
+    console.log(`No listings found with the name '${nameOfListing}'`);
+  }
+}
+
+async function findListingByCity(nameOfListing) {
+  const result = await User.find({ city: nameOfListing });
+  if (result.length > 0) {
+    console.log(result);
+  } else {
+    console.log(`No listings found with the city '${nameOfListing}'`);
+  }
+}
+
+async function findListingByAge(lowerlimit, upperLimit) {
+  const result = await User.find({
+    age: { $gte: lowerlimit, $lte: upperLimit },
+  });
+  if (result) {
+    console.log(result);
+  } else {
+    console.log(
+      `No listings found within age ${lowerlimit} and age ${upperLimit}.`
+    );
+  }
+}
